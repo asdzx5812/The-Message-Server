5e8855106e6f3ddd6e36ff34403ceaac3b2ab12e
simon <b06705059@ntu.edu.tw>
2021-06-03 17:32:02 +0800

    initialize

---
+++ src/main/java/org/foop/finalproject/themessageserver/TheMessageServerApplication.java
@@ -0,0 +1,13 @@
+package org.foop.finalproject.themessageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheMessageServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheMessageServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/foop/finalproject/themessageserver/TheMessageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.foop.finalproject.themessageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheMessageServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
