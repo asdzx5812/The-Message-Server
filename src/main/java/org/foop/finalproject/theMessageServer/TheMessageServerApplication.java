af41822d6f4dcb8e3ad8339bab58741cf65719db
simon <b06705059@ntu.edu.tw>
2021-06-10 15:36:38 +0800

    websocket

--- src/main/java/org/foop/finalproject/theMessageServer/TheMessageServerApplication.java
+++ src/main/java/org/foop/finalproject/theMessageServer/TheMessageServerApplication.java
@@ -2,9 +2,10 @@ package org.foop.finalproject.theMessageServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TheMessageServerApplication {
+public class TheMessageServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TheMessageServerApplication.class, args);
