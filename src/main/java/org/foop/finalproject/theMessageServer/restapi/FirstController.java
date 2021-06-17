e7b5b871ac14ce5911d4b136a7aed7a9eab31d55
simon <b06705059@ntu.edu.tw>
2021-06-17 17:48:54 +0800

    1

--- src/main/java/org/foop/finalproject/theMessageServer/restapi/FirstController.java
+++ src/main/java/org/foop/finalproject/theMessageServer/restapi/FirstController.java
@@ -1,13 +1,14 @@
 package org.foop.finalproject.theMessageServer.restapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
-    @RequestMapping("/hello")
-    public String firstControllerResponse(){
-        return "擇哥小ㄐㄐ";
+    @RequestMapping("/hello/{id}/ex/{game}")
+    public String firstControllerResponse(@PathVariable String id, @PathVariable String game){
+        return "擇哥小ㄐㄐ" + id + "/" + game;
     }
 }
