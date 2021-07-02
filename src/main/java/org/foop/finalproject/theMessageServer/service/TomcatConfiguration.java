3b80330d736dd9783eb94c25a0fca8136e2bb01e
simon <b06705059@ntu.edu.tw>
2021-07-02 21:12:08 +0800

    wss

---
+++ src/main/java/org/foop/finalproject/theMessageServer/service/TomcatConfiguration.java
@@ -0,0 +1,54 @@
+package org.foop.finalproject.theMessageServer.service;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.websocket.server.WsSci;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfiguration {
+
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(8080);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(56789);
+        return connector;
+    }
+
+    /**
+     * 建立wss协议接口
+     *
+     * @return
+     */
+    @Bean
+    public TomcatContextCustomizer tomcatContextCustomizer() {
+        System.out.println("init");
+        return new TomcatContextCustomizer() {
+            @Override
+            public void customize(Context context) {
+                System.out.println("init   customize");
+                context.addServletContainerInitializer(new WsSci(), null);
+            }
+
+        };
+    }
+
+
+}
